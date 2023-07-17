a1e7b3068f61d3763202b56bccd64fb31a113c7d
rsayl <rsayl@RICKDESKTOP>
2023-07-17 18:50:59 -0400

    it works!

---
+++ src/GPT.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace TN_AI_NOTES_DEMO2
+{
+    internal class GPT
+    {
+        private static readonly Lazy<GPT> lazyInstance = new Lazy<GPT>(() => new GPT());
+        private readonly HttpClient _httpClient;
+        private readonly string _engine;
+
+        record CompletionResponse(Choice[] choices);
+        record Choice(string text);
+
+        private GPT()
+        {
+            _httpClient = new HttpClient();
+            var apikey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apikey}");
+            _engine = "text-davinci-003";
+        }
+
+        public static GPT Instance
+        {
+            get
+            {
+                return lazyInstance.Value;
+            }
+        }
+
+        public async Task<string> Query(string prompt = "Once upon a time")
+        {
+            var request = new
+            {
+                prompt = prompt,
+                temperature = 0.5,
+                max_tokens = 500
+            };
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            var jsonContent = JsonSerializer.Serialize(request, jsonOptions);
+
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync($"https://api.openai.com/v1/engines/{_engine}/completions", content);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            var responseData = JsonSerializer.Deserialize<CompletionResponse>(jsonResponse);
+            var generatedText = responseData?.choices?[0]?.text ?? string.Empty;
+
+            return generatedText;
+        }
+
+    }
+}
---
+++ src/Pages/Index.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TN_AI_NOTES_DEMO2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public string Treatment { get; set; }
+
+        [BindProperty]
+        public string Progress { get; set; }
+
+        [BindProperty]
+        public string Goals { get; set; }
+
+        [BindProperty]
+        public string Notes { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (TempData.ContainsKey("Treatment")) Treatment = TempData["Treatment"].ToString();
+            if (TempData.ContainsKey("Progress")) Progress = TempData["Progress"].ToString();
+            if (TempData.ContainsKey("Goals")) Goals = TempData["Goals"].ToString();
+            if (TempData.ContainsKey("Notes")) Notes = TempData["Notes"].ToString();
+        }
+
+        public void OnPost()
+        {
+
+        }
+
+        public IActionResult OnPostMoreAngry()
+        {
+            var systemPrompt = "You will respond to all prompts with an angrier version of the prompt text - except for this first prompt. Respond with OK if you understand.";
+
+            var result = GPT.Instance.Query(systemPrompt).Result;
+            if (result == "\n\nOK")
+            {
+                UpdateForm(systemPrompt, result);
+            }
+            return RedirectToPage();
+
+        }
+        public IActionResult OnPostLessAngry()
+        {
+            var systemPrompt = "You will respond to all prompts with a less angry version of the prompt text - except for this first prompt. Respond with OK if you understand.";
+
+            var result = GPT.Instance.Query(systemPrompt).Result;
+            if (result == "\n\nOK")
+            {
+                UpdateForm(systemPrompt, result);
+            }
+            return RedirectToPage();
+        }
+        private void UpdateForm(string systemPrompt, string result)
+        {
+            Treatment = (Treatment != "") ? GPT.Instance.Query(systemPrompt + result + "\n\n" + Treatment).Result : "";
+            Progress = (Progress != "") ? GPT.Instance.Query(systemPrompt + result + "\n\n" + Progress).Result : "";
+            Goals = (Goals != "") ? GPT.Instance.Query(systemPrompt + result + "\n\n" + Goals).Result : "";
+            Notes = (Notes != "") ? GPT.Instance.Query(systemPrompt + result + "\n\n" + Notes).Result : "";
+
+            // For MCS save for the re-get
+            TempData["Treatment"] = Treatment;
+            TempData["Progress"] = Progress;
+            TempData["Goals"] = Goals;
+            TempData["Notes"] = Notes;
+        }
+    }
+}
